a171f33aca25a28223a1efbbd91e31ae786df53c
Ramkumar96 <sivaram0203@gmail.com>
2020-04-07 11:50:55 +0530

    Paddle and Ball Collisions

--- Pong_game/pong1.py
+++ Pong_game/pong1.py
@@ -38,26 +38,32 @@ ball.dx = 0.2
 ball.dy = -0.2
 
 
+
+
 # Function
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
     paddle_a.sety(y)
 
+
 def paddle_a_down():
     y = paddle_a.ycor()
     y -= 20
     paddle_a.sety(y)
+
 
 def paddle_b_up():
     y = paddle_b.ycor()
     y += 20
     paddle_b.sety(y)
 
+
 def paddle_b_down():
     y = paddle_b.ycor()
     y -= 20
     paddle_b.sety(y)
+
 
 # Keyboard Binding
 # listening to Keyboard
@@ -68,15 +74,13 @@ wn.onkeypress(paddle_a_down, "s")
 wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
 
-
 # main loop
 while True:
     wn.update()
 
     # Move The Ball
-    ball.setx(ball.xcor() +ball.dx)
+    ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
-
 
     # Border Checking
     if ball.ycor() > 290:
@@ -93,4 +97,13 @@ while True:
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
+        ball.dx *= -1
+
+    # paddle and ball collisions
+    if (340 < ball.xcor() < 350) and (paddle_b.ycor() + 40 > ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (-350 < ball.xcor() < -340) and (paddle_a.ycor() + 40 > ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
         ball.dx *= -1
