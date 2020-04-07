206ee3230eb36ad8c27a8beadbba3484ead084f4
Ramkumar96 <sivaram0203@gmail.com>
2020-04-07 09:58:54 +0530

    Pong game first commit

---
+++ Pong_game/pong1.py
@@ -0,0 +1,14 @@
+# Pong Game
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong Game")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+# main loop
+while True:
+    wn.update()
