daddacb0a8b53ba08d2bd72011c2ae37351f350c
Ramkumar96 <sivaram0203@gmail.com>
2020-04-08 20:48:30 +0530

    players

--- Tic_Tac_Toe/tic_tac_toe.py
+++ Tic_Tac_Toe/tic_tac_toe.py
@@ -29,7 +29,7 @@ def play_game():
 
     # game has ended
     if winner == "X" or winner == "O":
-        print(winner + "Won")
+        print(winner + " Won")
     elif winner is None:
         print("Tie")
 
@@ -38,7 +38,7 @@ def handle_turn(player):
     position = input("Choose a position from (1-9):")
     position = int(position) - 1
 
-    board[position] = "X"
+    board[position] = player
     display_board()
 
 
@@ -58,11 +58,11 @@ def check_for_winner():
     diagonal_winner = check_diagonals()
 
     if row_winner:
-        winner = row_winner()
+        winner = row_winner
     elif column_winner:
-        winner = column_winner()
+        winner = column_winner
     elif diagonal_winner:
-        winner = diagonal_winner()
+        winner = diagonal_winner
     else:
         winner = None
     return
@@ -76,13 +76,50 @@ def check_rows():
     row_2 = board[3] == board[4] == board[5] != "_"
     row_3 = board[6] == board[7] == board[8] != "_"
 
+    if row_1 or row_2 or row_3:
+        game_still_going =False
+
+    if row_1:
+        return board[0]
+    if row_2:
+        return board[3]
+    if row_3:
+        return board[6]
 
     return
 
 def check_columns():
+    global game_still_going
+
+    column_1 = board[0] == board[3] == board[6] != "_"
+    column_2 = board[1] == board[4] == board[7] != "_"
+    column_3 = board[2] == board[5] == board[8] != "_"
+
+    if column_1 or column_2 or column_3:
+        game_still_going = False
+
+    if column_1:
+        return board[0]
+    if column_2:
+        return board[1]
+    if column_3:
+        return board[2]
     return
 
 def check_diagonals():
+    global game_still_going
+
+    diagonals_1 = board[0] == board[4] == board[8] != "_"
+    diagonals_2 = board[6] == board[4] == board[2] != "_"
+
+
+    if diagonals_1 or diagonals_2:
+        game_still_going = False
+
+    if diagonals_1:
+        return board[0]
+    if diagonals_2:
+        return board[6]
     return
 
 def check_if_tie():
